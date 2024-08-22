610dc8590879819b09d53233cb5fc3f633e9906d
PritamUem <163764348+PritamUem@users.noreply.github.com>
2024-08-22 23:31:15 +0530

    Update linkedlist2.c

--- linkedlist2.c
+++ linkedlist2.c
@@ -1,51 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
-
 struct Node{
     int data;
     struct Node* next;
-};
+};struct Node *head ,*newNode;
 
 void insert(struct Node **head){
-    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
-    if(newNode == NULL){
-        printf("Memory allocation failed\n");
-        return;
-    }
-    printf("Enter the data you want to enter: ");
-    if(scanf("%d", &newNode->data) != 1){
-        printf("Invalid input\n");
-        free(newNode);
-        return;
-    }
-    newNode->next = *head;
-    *head = newNode;
+newNode=(struct Node*)malloc(sizeof(struct Node));
+if(newNode==NULL){
+    printf("failed");
+    return;
 }
-
+printf("enter the data you wanrt to enter:");
+scanf("%d",&newNode->data);
+newNode->next=*head;
+*head=newNode;
+}
 void print(struct Node *head){
-    struct Node *temp = head;
-    while(temp != NULL){
-        printf("%d ", temp->data);
-        temp = temp->next;
+    struct Node *temp=head;
+    while(temp!=NULL){
+        printf("%d",temp->data);
+        
+        temp=temp->next;
     }
     printf("\n");
 }
-
-void freeList(struct Node *head){
-    struct Node *temp = head;
-    while(temp != NULL){
-        struct Node *next = temp->next;
-        free(temp);
-        temp = next;
-    }
-}
-
 int main(){
-    struct Node *head = NULL;
-    insert(&head);
-    insert(&head);
-    insert(&head);
-    print(head);
-    freeList(head);
-    return 0;
+struct Node *head=NULL;
+insert(&head);
+insert(&head);
+insert(&head);
+//passing the adress of head through &head;
+print(head);
+return 0;
 }
